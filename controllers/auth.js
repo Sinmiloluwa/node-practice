1bec055801de1698afb25fc9ac40a783589cf6b3
Sinmiloluwa <mofeoluwae@gmail.com>
2025-02-12 12:52:30 +0100

    fix signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
 
 export function getLogin(req, res, next) {
     res.render('auth/login', {
@@ -24,4 +25,38 @@ export function logout(req, res, next) {
         console.log(err);
         res.redirect('/')
     });
+}
+
+export function getSignup(req, res, next) {
+    res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: req.session.isLoggedIn,
+    })
+}
+
+export function postSignup(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = req.body.username;
+    User.findOne({email: email})
+    .then(userDoc => {
+        if (userDoc) {
+            return res.redirect('/signup');
+        }
+        return bcrypt.hash(password, 12)
+    }).then(hashedPassword => {
+        const user = new User({
+            email: email,
+            password: hashedPassword,
+            name: username,
+            cart: {items: []}
+        });
+        return user.save();
+        res.redirect('/login');
+    })
+    .then(result => {
+        res.redirect('/login');
+    })
+    .catch(err => console.log(err));
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import {getLogin, postLogin, logout} from '../controllers/auth.js';
+import {getLogin, postLogin, logout, getSignup, postSignup} from '../controllers/auth.js';
 
 const router = express.Router();
 
@@ -9,6 +9,10 @@ router.get('/login', getLogin);
 router.post('/login', postLogin);
 
 router.post('/logout', logout);
+
+router.get('/signup', getSignup);
+
+router.post('/signup', postSignup);
 
 
 export default router;
